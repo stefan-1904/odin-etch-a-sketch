235db8766886f05e98bd0e93c6838f9122d608b5
Stefan <sminns1230@gmail.com>
2024-01-12 20:35:59 +0000

    Add hover color to grid

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 function createGrid(rows, cols) {
-    const container = document.getElementById('container');
-
-    // Create rows
-    for (let r = 0; r < rows; r++) {
-        const row = document.createElement('div');
-        row.classList.add('grid-row');
-
-        // Create cells inside each row
-        for (let c = 0; c < cols; c++) {
-            const cell = document.createElement('div');
-            cell.classList.add('grid-column');
-            
-            // Append cells to each row
-            row.appendChild(cell)
-        }
-        // Append each row to the container
-        container.appendChild(row);
-    }
+	const container = document.getElementById('container');
+	for (let r = 0; r < rows; r++) {
+		const row = document.createElement('div');
+		row.classList.add('grid-row');
+		for (let c = 0; c < cols; c++) {
+			const cell = document.createElement('div');
+			cell.classList.add('grid-column');
+			row.appendChild(cell)
+		}
+		container.appendChild(row);
+	}
 }
 
 createGrid(16,16);
+
+function gridHover() {
+	let cells = document.querySelectorAll('.grid-column');
+	cells.forEach(cell => {
+		cell.addEventListener('mouseover', () => {
+			cell.style.backgroundColor = 'black'
+		});
+	});
+}
+
+gridHover()
