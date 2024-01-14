44d10d1a4fd38688d39f3a76e9975dba5a90288a
Stefan <sminns1230@gmail.com>
2024-01-14 19:00:03 +0000

    Add consistent grid sizing & several CSS attributes to create a more refined look

--- app.js
+++ app.js
@@ -1,48 +1,50 @@
 function createGrid(rows, cols) {
-	const container = document.getElementById('container');
-	for (let r = 0; r < rows; r++) {
-		const row = document.createElement('div');
-		row.classList.add('grid-row');
-		for (let c = 0; c < cols; c++) {
-			const cell = document.createElement('div');
-			cell.classList.add('grid-column');
-			row.appendChild(cell);
-		}
-		container.appendChild(row);
-	}
+    const container = document.getElementById('container');
+
+    for (let r = 0; r < rows; r++) {
+        const row = document.createElement('div');
+        row.classList.add('grid-row');
+
+        for (let c = 0; c < cols; c++) {
+            const cell = document.createElement('div');
+            cell.classList.add('grid-column');
+            row.appendChild(cell);
+        }
+        container.appendChild(row);
+    }
 }
 
 createGrid(16, 16);
 
 function gridHover() {
-	const cells = document.querySelectorAll('.grid-column');
-	cells.forEach(cell => {
-		cell.addEventListener('mouseover', () => {
-			cell.style.backgroundColor = 'black';
-		});
-	});
+    const container = document.getElementById('container');
+
+    container.addEventListener('mouseover', (event) => {
+        if (event.target.classList.contains('grid-column')) {
+            event.target.style.backgroundColor = 'black';
+        }
+    });
 }
 
 gridHover();
+
+function removeGrid() {
+    const container = document.getElementById('container');
+    container.innerHTML = '';
+}
 
 function gridButtonPrompt() {
     const gridBtn = document.querySelector('#size-btn');
     gridBtn.addEventListener('click', () => {
         let userInput = prompt('Enter new grid size from 1-64:');
         userInput = parseInt(userInput);
-        if (userInput < 1 || userInput > 64) {
+        if (isNaN(userInput) || userInput < 1 || userInput > 64) {
             alert('Invalid. Choose a number between 1-64');
         } else {
-            gridSize(userInput);
+            removeGrid();
+            createGrid(userInput, userInput);
         }
     });
 }
 
 gridButtonPrompt();
-
-function gridSize(userInput) {
-    let gridCells = document.querySelectorAll('.grid-row', 'grid-column');
-    gridCells.forEach(gridCell => gridCell.remove());
-    createGrid(userInput, userInput);
-	gridHover();
-}
